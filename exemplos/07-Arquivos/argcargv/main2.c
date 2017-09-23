29fc489c2bedae3568d52f4417c1d6f3967e404b
Flavio Figueiredo <flaviovdf@gmail.com>
2017-09-23 11:18:55 -0300

    Argc e Argv examples

---
+++ exemplos/07-Arquivos/argcargv/main2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("Recibi %d argumentos\n", argc);
+  for (int i = 0; i < argc; i++)
+    printf("Argumento %d foi %s\n", i, argv[i]);
+  return 0;
+}
